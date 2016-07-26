aaa821ee2fd95a88d00b81e0050c18b954c4808e
Sam Sataponn <sam@abctech-thailand.com>
2016-07-26 17:10:37 +0700

    Edit the rest of the callback declarations

--- test/test.js
+++ test/test.js
@@ -2,8 +2,8 @@ const assert = require('chai').assert;
 const http = require('http');
 
 describe('GET /', function() {
-    it('should respond with status 200', (done) => {
-        http.get('http://localhost:3000/', (res) => {
+    it('should respond with status 200', function(done) {
+        http.get('http://localhost:3000/', function(res) {
             assert.equal(res.statusCode, 200);
             done();
         });
