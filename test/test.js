4a63ad3c440f71c27a7c0730ce63bf1098f0a01e
Sam Sataponn <sam@abctech-thailand.com>
2016-07-26 17:28:50 +0700

    Edit test.js back to the strict mode

--- test/test.js
+++ test/test.js
@@ -1,9 +1,11 @@
+'use strict';
+
 const assert = require('chai').assert;
 const http = require('http');
 
-describe('GET /', function() {
-    it('should respond with status 200', function(done) {
-        http.get('http://localhost:3000/', function(res) {
+describe('GET /', () => {
+    it('should respond with status 200', (done) => {
+        http.get('http://localhost:3000/', (res) => {
             assert.equal(res.statusCode, 200);
             done();
         });
