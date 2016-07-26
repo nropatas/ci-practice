483debe97672ac16a086fb737625878600aa9bb0
Sam Sataponn <sam@abctech-thailand.com>
2016-07-26 17:09:01 +0700

    Change callback function declaration
    - An error 'unexpected token )' occurred on circleci.

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 const assert = require('chai').assert;
 const http = require('http');
 
-describe('GET /', () => {
+describe('GET /', function() {
     it('should respond with status 200', (done) => {
         http.get('http://localhost:3000/', (res) => {
             assert.equal(res.statusCode, 200);
