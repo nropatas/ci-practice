272a317ada075503d553df2964e2b6531c7e4360
Sam Sataponn <sam@abctech-thailand.com>
2016-07-26 16:54:52 +0700

    Install mocha and add test script

---
+++ test/test.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const assert = require('chai').assert;
+const http = require('http');
+
+describe('GET /', () => {
+    it('should respond with status 200', (done) => {
+        http.get('http://localhost:3000/', (res) => {
+            assert.equal(res.statusCode, 200);
+            done();
+        });
+    });
+});
