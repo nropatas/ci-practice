8e5566a1dd14a75b882679df060a026695999e8f
Sam Sataponn <sam@abctech-thailand.com>
2016-07-26 16:24:03 +0700

    Edit port setting

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const app = express();
 
-app.set('port', 8080);
+app.set('port', process.env.PORT || 3000);
 
 app.get('/', (req, res) => {
     res.send('Hello World');
